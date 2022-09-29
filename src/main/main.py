ca64b4f3c955221206e25d2e892e45524c1142fd
mikeshort123 <mike.short.nh@gmail.com>
2022-09-29 19:28:27 +0100

    Dynamically generate sides

--- src/main/main.py
+++ src/main/main.py
@@ -8,35 +8,14 @@ def main():
     pygame.init()
 
     WIDTH, HEIGHT = 640, 480
+    ZOOM = 400
 
     display = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
-    corners = np.array([
-        [ 1, 1, 1],
-        [ 1, 1,-1],
-        [ 1,-1, 1],
-        [ 1,-1,-1],
-        [-1, 1, 1],
-        [-1, 1,-1],
-        [-1,-1, 1],
-        [-1,-1,-1]
-    ])
+    d = 3
 
-    sides = np.array([
-        [0,1],
-        [0,2],
-        [0,4],
-        [1,3],
-        [1,5],
-        [2,3],
-        [2,6],
-        [3,7],
-        [4,5],
-        [4,6],
-        [5,7],
-        [6,7]
-    ])
+    corners, sides = generate_objects(1, d)
 
     r=0
 
@@ -49,12 +28,21 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        rotation = np.array([
+        rotation_a = np.array([
             [math.cos(r), 0,-math.sin(r)],
             [0, 1, 0],
             [math.sin(r), 0, math.cos(r)]
 
         ])
+
+        rotation_b = np.array([
+            [math.cos(r), -math.sin(r), 0],
+            [math.sin(r),  math.cos(r), 0],
+            [0, 0, 1]
+
+        ])
+
+        rotation = rotation_a @ rotation_b
 
         r += 0.01
 
@@ -64,7 +52,7 @@ def main():
         points_xy = transformed[:, 0:2]
         points_z = transformed[:, 2:3]
 
-        screen_pos = WIDTH * (points_xy / points_z) + np.array([WIDTH, HEIGHT]) / 2
+        screen_pos = ZOOM * (points_xy / points_z) + np.array([WIDTH, HEIGHT]) / 2
 
 
         for point in screen_pos:
@@ -77,5 +65,48 @@ def main():
 
         pygame.display.update()
         clock.tick(60)
+
+def point_generator(n, b, d):
+
+    l = []
+    for i in range(d):
+
+        l.append((n//(b**i))%b)
+
+    return l
+
+def point_index(l, b, d):
+    t = 0
+    for i, p in enumerate(l):
+        t += p * (b ** i)
+
+    return t
+
+def generate_objects(squares, dimensions):
+
+    points = []
+    edges = []
+
+    n_points = squares+1
+
+
+
+    for i in range(n_points**dimensions):
+        n = point_generator(i, n_points, dimensions)
+
+        points.append(n)
+
+        for index, value in enumerate(n):
+
+            if n[index] >= squares:
+                continue
+
+            end = n[:]
+            end[index] += 1
+            end_index = point_index(end, n_points, dimensions)
+            edges.append([i, end_index])
+
+    return np.array(points) - squares/2, np.array(edges)
+
 
 if __name__ == '__main__': main()
